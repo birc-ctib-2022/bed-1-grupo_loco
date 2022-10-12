2f34cce99f7f788cd0bc270a568a8f1cfbb130ce
Silas-tander <silas@tanderup.net>
2022-10-12 14:15:37 +0200

    Format test UNO!!!

--- src/format_bed_test.py
+++ src/format_bed_test.py
@@ -1,10 +1,15 @@
 # This directory will be checked with pytest. It will examine
 # all files that start with test_*.py and run all functions with
 # names that start with test_
+from format_bed import main
+import filecmp
+from difflib import Differ
 
-def test_1984():
-    assert 2 + 2 == 4
+exp_output = "data/output.bed"
+input = "data/output.bed"
+output = main(input)
 
+def format_test():
+    assert filecmp.cmp(output,exp_output)
 
-def test_line
 
