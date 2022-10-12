f3786373d8c2126647027db6e823296afc603297
Silas-tander <silas@tanderup.net>
2022-10-12 14:46:23 +0200

    Format_test update 1

--- src/format_bed_test.py
+++ src/format_bed_test.py
@@ -1,15 +1,17 @@
 # This directory will be checked with pytest. It will examine
 # all files that start with test_*.py and run all functions with
 # names that start with test_
-#from format_bed import main
-#import filecmp
+from format_bed import main
+import filecmp
 #from difflib import Differ
 
 #exp_output = "data/output.bed"
 #input = "data/output.bed"
 #output = main(input)
 
-#def format_test():
-#    assert filecmp.cmp(output,exp_output)
+def format_test():
+    with open('data/input.bed') as input, open('data/output.bed') as output:
+        expected = main(input)
+        assert filecmp.cmp(expected,output)
 
 
