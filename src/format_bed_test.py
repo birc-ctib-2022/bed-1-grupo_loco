44b73524e7ee70df8e415957be8efbb73de847bd
Silas-tander <silas@tanderup.net>
2022-10-12 14:28:43 +0200

    Test commit

--- src/format_bed_test.py
+++ src/format_bed_test.py
@@ -1,15 +1,15 @@
 # This directory will be checked with pytest. It will examine
 # all files that start with test_*.py and run all functions with
 # names that start with test_
-from format_bed import main
-import filecmp
-from difflib import Differ
+#from format_bed import main
+#import filecmp
+#from difflib import Differ
 
-exp_output = "data/output.bed"
-input = "data/output.bed"
-output = main(input)
+#exp_output = "data/output.bed"
+#input = "data/output.bed"
+#output = main(input)
 
-def format_test():
-    assert filecmp.cmp(output,exp_output)
+#def format_test():
+#    assert filecmp.cmp(output,exp_output)
 
 
